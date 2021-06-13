505a3868df286475281a479bffe53a67b0bbfa05
unknown <shalamov2054@mail.ru>
2021-06-14 01:42:42 +0500

    fix

--- lib/models/sections/Section.dart
+++ lib/models/sections/Section.dart
@@ -1,16 +1,20 @@
 import 'package:forest_island/models/product/Product.dart';
 
 class Section{
+  final int id;
+
   final String sectionTitle;
 
   final List<Product> products;
 
   Section({
     this.products,
+    this.id,
     this.sectionTitle
   });
 
   factory Section.fromJson(Map<String, dynamic> json) => Section(
+    id: json["id"],
     sectionTitle: json["sectionTitle"],
     products: (json["products"] as List<dynamic>).map((item) => Product.fromJson(item)).toList(),
   );
